d985e7f1835d8e14828a3780f0eb8b934ab808b8
Luciano Borges <lucianosantosborges@gmail.com>
2018-04-04 10:03:04 -0300

    Initializing project

---
+++ src/main/java/com/heart/echo/EchocardiogramApplication.java
@@ -0,0 +1,12 @@
+package com.heart.echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchocardiogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchocardiogramApplication.class, args);
+	}
+}
